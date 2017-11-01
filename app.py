6ce478ed05e7b3457a436975c8467d0ebc51b3d5
Chris <Ginrawc@users.noreply.github.com>
2017-10-31 22:44:43 -0400

    update app

--- app.py
+++ app.py
@@ -46,4 +46,6 @@ def mnist():
     return jsonify(results = [predictions.tolist(),url_for('static',filename=create_graph(labels,predictions.tolist()[0]))])
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
